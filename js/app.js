b009526b25f972bcac6cd85fe48d2b6da7fe7606
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-10-08 20:51:55 +0000

    Completed exercise-2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-// Write your code here!
+var imgSelect = $("img");
+
+imgSelect.each(function(){
+    
+    var $img = $(this);
+    
+    $img.on("click", function(){
+        
+        var $url = $(this).attr("src");
+        var $alt = $(this).attr("alt");
+        
+        $("div.overlay").css('display','flex');
+        $("div.overlay-img").append('<img src="'+$url+'"><p>'+$alt+'</p>');
+        
+        $("div.overlay").on("click", function(){
+            
+            $(this).css('display','none');
+            $("div.overlay-img img, p").remove();
+        });
+    });
+});
