476356bb740c53e80f9f891e8dab8ac5eb224b30
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-10-08 21:31:38 +0000

    Completed exercise-3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-// Write your code here!
+$('#signup-form').on('submit', function(event) {
+    
+    var $password = $("input[name='password']").val();
+    var $confirm = $("input[name='password2']").val();
+    
+    if($password.length < 10){
+        alert("Please select a password of at least 10 characters.");
+        event.preventDefault();
+    }
+    else {
+        
+        if($password !== $confirm){
+            
+        }
+        else {
+            alert("Passwords not matching.");
+            event.preventDefault();
+        }
+    }
+});
