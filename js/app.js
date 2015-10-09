60cc145045e5d47f0071b138b60f1d6ba2f0417c
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-10-09 00:01:05 +0000

    Completed exercise-5

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-// Write your code here!
+function setIntervalAndExecute(fn, t) {
+    fn();
+    return window.setInterval(fn, t); 
+}
+    
+function getIss() { $.getJSON('http://api.open-notify.org/iss-now.json?callback=?').then(
+    function(res) {
+        
+        var $lat = res.iss_position.latitude;
+        var $lng = res.iss_position.longitude;
+        var $mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+$lat+","+$lng+"&markers=color:black|label:X|"+$lat+","+$lng+"&zoom=3&size=500x500&&key=AIzaSyCFYgUuLsL7hhJEXH2FKSkbqlXb5DRUfmc";
+
+        $("#iss-position").attr("src",$mapUrl);
+    }
+)}
+
+setIntervalAndExecute(getIss,30000);
+
+
+
+
